46198978e1e8995f31ac332204effe31ae7f7dc4
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-08-20 22:22:15 +0300

    rename

--- src/pages/HomePages/HomePages.jsx
+++ src/pages/HomePages/HomePages.jsx
@@ -1,6 +1,7 @@
 import { getTrending } from 'api';
+import Movies from 'components/Movies/Movies';
 import { useState, useEffect } from 'react';
-import Home from 'components/Home/Home';
+
 
 export default function HomePages() {
   const [movies, setMovies] = useState([]);
@@ -8,20 +9,16 @@ export default function HomePages() {
   useEffect(() => {
     getTrending()
       .then(data => {
-        // console.log('Data from getTrending:', data);
         setMovies(data);
       })
       .catch(error => {
-        // console.error('Error fetching trending movies:', error);
       });
   }, []);
-
-  // console.log('Movies state:', movies);
 
   return (
     <main>
       <h1>Trending today</h1>
-      <ul>{movies.length !== 0 && <Home movies={movies} />}</ul>
+      <ul>{movies.length !== 0 && <Movies movies={movies} />}</ul>
     </main>
   );
 }
